4a755360f524b0ecf545bdb00c180eb0db5a37e0
99003169 <75420456+99003169@users.noreply.github.com>
2020-12-15 11:51:34 +0530

    Create TCP_SERVER.py

---
+++ TCP_SERVER.py
@@ -0,0 +1,27 @@
+#tcp server
+
+import socket
+import os
+import time
+
+addr = ("127.0.0.1", 1510)  #tuple
+
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.bind(addr)
+mysock.listen()               #listening mode
+
+#time.sleep(10) 
+
+(clisock,addr) = mysock.accept()
+print("got a client conn from ", addr)
+#for testing, telnet 127.0.0.1.1500
+
+
+data = clisock.recv(64)
+print("client sent:", data.decode('utf-8'))
+
+mystr = "thank you"
+clisock.sendall(mystr.encode('utf-8'))
+
+clisock.close()
+mysock.close()
